a0be7db21feeaba9a0708576b782a241bd70c0c1
Chen Mualem <chenmu10@gmail.com>
2018-04-11 16:54:50 +0300

    changed navbar class to navigation

--- js/cmps/navbar.js
+++ js/cmps/navbar.js
@@ -1,11 +1,10 @@
 
 export default {
     template: `
-    <section class="navbar">
-      <header class="">
+    <section class="navigation">
+      <header>
                
                 <h1>Appsus</h1>
-
                 <router-link to="/">Home</router-link>
                 <router-link to="/about">About</router-link>
                 <router-link to="/email">Email</router-link>
--- js/services/storage.service.js
+++ js/services/storage.service.js
@@ -1,12 +1,12 @@
 
-
 function store(key, any) {
     localStorage[key] = JSON.stringify(any);
+    return Promise.resolve();
 }
 
 function load(key) {
     var str = localStorage[key] || 'null';
-    return JSON.parse(str);
+    return Promise.resolve(JSON.parse(str));
 }
 
 export default {
