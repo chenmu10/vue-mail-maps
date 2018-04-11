d7e8536bd02923fb07eb5d3ebc057afc65f2b042
Chen Mualem <chenmu10@gmail.com>
2018-04-11 16:36:02 +0300

    added navbar component

---
+++ js/cmps/navbar.js
@@ -0,0 +1,18 @@
+
+export default {
+    template: `
+    <section class="navbar">
+      <header class="">
+               
+                <h1>Appsus</h1>
+
+                <router-link to="/">Home</router-link>
+                <router-link to="/about">About</router-link>
+                <router-link to="/email">Email</router-link>
+                <router-link to="/map">Map</router-link>
+                <router-link to="/notes">Notes</router-link>
+            </header>
+           
+    </section>
+    `
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 /* */
 import router from './routes.js'
 import userMsg from './cmps/user-msg.js'
+import navbar from './cmps/navbar.js'
 
 
 new Vue({
     el: '#app',
     router,
     components: {
+      navbar,
       userMsg
     }
   })
