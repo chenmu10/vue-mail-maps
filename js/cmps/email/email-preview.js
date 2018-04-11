23305ee18e900620580d878d321e8260501e6c30
Tal Amiel <amielsosa@gmail.com>
2018-04-11 20:38:56 +0300

    email preview added

--- js/cmps/email/email-preview.js
+++ js/cmps/email/email-preview.js
@@ -2,11 +2,46 @@ export default {
     props: {email:{type: Object, required: true}},
    
    template:`
-     <section>
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://bulma.io/images/placeholders/128x128.png">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        {{email.subject}}
+        <br>
+        {{email.body}}
+      </p>
+    </div>
+    <nav class="level is-mobile">
+      <div class="level-left">
+        <a class="level-item">
+          <span class="icon is-small"><i class="fas fa-reply"></i></span>
+        </a>
+        <a class="level-item">
+          <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+        </a>
+        <a class="level-item">
+          <span class="icon is-small"><i class="fas fa-heart"></i></span>
+        </a>
+      </div>
+    </nav>
+  </div>
+  <div class="media-right">
+  {{email.sentAt}}
+  </div>
+</article>
+     <!-- <section>
         <h1>{{email.subject}}</h1>
         <p> read: {{email.isRead}} </p>
-    </section>
+    </section> -->
     `
 }
+
+
+
 
 
