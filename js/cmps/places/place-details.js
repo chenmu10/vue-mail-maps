95f81fa77ecd7b804b479bd9a6fc5c3a485ca0e7
Chen Mualem <chenmu10@gmail.com>
2018-04-12 21:16:45 +0300

    changed places-app

--- js/cmps/places/place-details.js
+++ js/cmps/places/place-details.js
@@ -1,19 +1,38 @@
 export default {
-
+    props: {place:{type: Object, required: true}},
+    data(){
+        return {
+           
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+       
+        deletePlace() {
+            console.log(this.place);
+            this.$emit('deletePlace', this.place)
+        },
+        editPlace() {
+            console.log(this.place);
+            this.$emit('addPlace', this.place)
+        },
+    },
     template: `
     <section class="section">
     <div class="place-details">
         <h1 class="title is-2">place-details</h1>
-        <form>
+        <form @submit.prevent>
             name:
-            <input type="text"> lat,lng:
+            <input type="text" :value="place.name"> lat,lng:
             <input type="text" disabled> description:
             <input type="text"> tag:
             <input type="text"> photos:
             <input type="text">
-            <button type="submit">Update</button>
-            <button>Delete</button>
-            <button>Add to my places</button>
+            <button type="submit" @click="editPlace">Update</button>
+            <button @click="deletePlace">Delete</button>
+            <button @click="editPlace">Add to my places</button>
         </form>
     </div>
     </section>
