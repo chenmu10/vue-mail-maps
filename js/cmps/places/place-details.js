018c8958020f797e8f7edb18b4e520fb7ef2944d
chenmu10 <chenmu10@gmail.com>
2018-04-13 18:33:08 +0300

    updated place details

--- js/cmps/places/place-details.js
+++ js/cmps/places/place-details.js
@@ -7,7 +7,7 @@ export default {
     },
     data() {
         return {
-
+            currPlace: this.place
         }
     },
     computed: {
@@ -16,41 +16,43 @@ export default {
     methods: {
 
         deletePlace() {
-            console.log('place to delete:', this.place);
-            this.$emit('delete', this.place)
+            console.log('place to delete:', this.currPlace);
+            this.$emit('delete', this.currPlace);
         },
         editPlace() {
-            console.log('place to edit', this.place);
-            this.$emit('edit', this.place)
+            console.log('place to edit', this.currPlace);
+            this.$emit('edit', this.currPlace);
         },
     },
     template: `
     <section class="section">
     <div class="place-details">
         <h1 class="title is-2">place-details</h1>
+        <p>crrPlace:{{ currPlace }}</p>
+        <p>props Place:{{ place }}</p>
         <form @submit.prevent>
             <div>  Name:
-                  <input type="text" v-model="place.name" >
+                  <input type="text" :value="currPlace.name" >
             </div>
              <div>Address:
-                  <input type="text" v-model="place.address">
+                  <input type="text" :value="currPlace.address">
             </div>
 
             <div>
              lat,lng:
-            <input type="text"  v-model="place.coords.lat">
+            <input type="text"  :value="currPlace.coords.lat">
             </div>
             <div>
              description:
-             <textarea  rows="3"  v-model="place.desc"></textarea>
+             <textarea  rows="3"  :value="currPlace.desc"></textarea>
              </div>
              <div>
              tag:
-            <input type="text"  v-model="place.tag">
+            <input type="text"  :value="currPlace.tag">
             </div>
             <div>
              photos:
-            <input type="text"  v-model="place.imgs[0]">
+            <input type="text"  :value="currPlace.imgs[0]">
             </div>
             <button @click.stop="editPlace">Update</button>
             <button @click.stop="deletePlace">Delete</button>
